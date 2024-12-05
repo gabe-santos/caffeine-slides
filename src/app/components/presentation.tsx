656fc61b95e3215a18b30d33584a808ec562c869
Gabe Santos <28gabe@gmail.com>
2024-12-05 10:24:56 -0800

    test deploy

--- src/app/components/presentation.tsx
+++ src/app/components/presentation.tsx
@@ -3,6 +3,10 @@ import { useEffect, useRef } from "react";
 import Reveal from "reveal.js";
 import "reveal.js/dist/reveal.css";
 import "@/app/my-theme.css";
+import Image from "next/image";
+
+import caffeineStructure from "@/app/images/Caffeine_structure.svg.png";
+import caffeinePure from "@/app/images/caffeine-pure.jpeg";
 
 export default function Presentation() {
   const deckDivRef = useRef<HTMLDivElement>(null); // reference to deck container div
@@ -37,12 +41,34 @@ export default function Presentation() {
   return (
     // Your presentation is sized based on the width and height of
     // our parent element. Make sure the parent is not 0-height.
-    <div className="reveal text-xl" ref={deckDivRef}>
+    <div className="reveal" ref={deckDivRef}>
       <div className="slides">
         <section>
-          <h1>Caffeine</h1>Slide 1
+          <h1>Caffeine</h1>
+          <p>Gabe Santos</p>
         </section>
-        <section>Slide 2</section>
+        <section data-auto-animate>
+          <h1 className="text-left">What even is Caffeine?</h1>
+        </section>
+        <section
+          data-auto-animate
+          className="flex border items-center justify-between w-full gap-4"
+        >
+          <h1 className="text-left text-7xl w-full">What even is Caffeine?</h1>
+
+          <div className="r-stack w-full">
+            <Image
+              className="fragment"
+              src={caffeineStructure}
+              alt="caffeine chemical structure"
+            />
+            <Image
+              className="fragment"
+              src={caffeinePure}
+              alt="caffeine chemical structure"
+            />
+          </div>
+        </section>
       </div>
     </div>
   );
